ec88f2b71dd6848d20d00e9919c5560b992d8ef9
falcon128 <pospisil.jan09@gmail.com>
2026-03-28 16:36:46 +0100

    add calculator.css and calculator.js

---
+++ calculator.js
@@ -0,0 +1,63 @@
+document.getElementById("1").addEventListener("click", function() {
+    alert("Tlačítko 1 bylo stisknuto!");
+})
+document.getElementById("2").addEventListener("click", function() {
+    alert("Tlačítko 2 bylo stisknuto!");
+}
+)
+document.getElementById("3").addEventListener("click", function() {
+    alert("Tlačítko 3 bylo stisknuto!");
+}
+)
+document.getElementById("4").addEventListener("click", function() {
+    alert("Tlačítko 4 bylo stisknuto!");
+}
+)
+document.getElementById("5").addEventListener("click", function() {
+    alert("Tlačítko 5 bylo stisknuto!");
+}
+)
+document.getElementById("6").addEventListener("click", function() {
+    alert("Tlačítko 6 bylo stisknuto!");
+}
+)
+document.getElementById("7").addEventListener("click", function() {
+    alert("Tlačítko 7 bylo stisknuto!");
+}
+)
+document.getElementById("8").addEventListener("click", function() {
+    alert("Tlačítko 8 bylo stisknuto!");
+}
+)
+document.getElementById("9").addEventListener("click", function() {
+    alert("Tlačítko 9 bylo stisknuto!");
+}
+)
+document.getElementById("0").addEventListener("click", function() {
+    alert("Tlačítko 0 bylo stisknuto!");
+}
+)
+document.getElementById("plus").addEventListener("click", function() {
+    alert("Tlačítko + bylo stisknuto!");
+}
+)
+document.getElementById("minus").addEventListener("click", function() {
+    alert("Tlačítko - bylo stisknuto!");
+}
+)
+document.getElementById("multiply").addEventListener("click", function() {
+    alert("Tlačítko * bylo stisknuto!");
+}
+)
+document.getElementById("divide").addEventListener("click", function() {
+    alert("Tlačítko / bylo stisknuto!");
+}
+)
+document.getElementById("equals").addEventListener("click", function() {
+    alert("Tlačítko = bylo stisknuto!");
+}
+)
+document.getElementById("clear").addEventListener("click", function() {
+    alert("Tlačítko C bylo stisknuto!");
+}
+)
